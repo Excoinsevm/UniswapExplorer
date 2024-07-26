7cb476f9c40017b714d42e15403b877019fa0486
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-07-26 11:32:46 +0100

    Update connectors.js

--- src/connectors.js
+++ src/connectors.js
@@ -2,7 +2,7 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 
 export const injectedConnector = new InjectedConnector({
   supportedChainIds: [
-    1, // Mainet
+    7171, // Mainet
     3, // Ropsten
     4, // Rinkeby
     5, // Goerli
